d8825bf2696f5a1093609869781894ed36e55f09
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 14:19:24 +0530

    adding RATSNode class

--- index.ts
+++ index.ts
@@ -132,3 +132,45 @@ class Animator {
         }
     }
 }
+
+class RATSNode {
+
+    prev : RATSNode 
+    next : RATSNode 
+    state : State = new State()
+     
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RATSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRATSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : RATSNode {
+        var curr : RATSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
