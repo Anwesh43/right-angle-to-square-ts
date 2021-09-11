2bc38db1b06255992884c966cca7fa03701234b6
Anwesh43 <anweshthecool0@gmail.com>
2021-09-11 14:29:57 +0530

    adding RightAngleToSquare class

--- index.ts
+++ index.ts
@@ -174,3 +174,26 @@ class RATSNode {
         return this 
     }
 }
+
+class RightAngleToSquare {
+
+    curr : RATSNode = new RATSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
